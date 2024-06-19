5c8d7fdcf804f17ddc8351698e1eff00faffcf19
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2024-06-18 22:38:34 -0300

    Initial files.

---
+++ src/blocodemo2/BlocoDemo2.java
@@ -0,0 +1,11 @@
+package blocodemo2;
+
+public class BlocoDemo2 {
+
+    public static void main(String[] args) {
+        // Instruções do método main.
+        int n1 = 2;
+        System.out.println("O valor de n1 é " + n1); // Não compila.
+    } // Fim do bloco do método main.
+  
+}
